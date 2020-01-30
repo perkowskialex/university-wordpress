b883354a19de167fd8192f8ac693f5e5540c9a3a
Alex Perkowski <perkowskialex@gmail.com>
2020-01-30 17:09:48 -0600

    starting up the project

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php 
+
+get_header();
+
+while(have_posts()){
+    the_post(); ?>
+    <h1>this is a page, not a post</h1>
+    <h2><?php the_title() ?></h2>
+    <?php the_content() ?>
+    <hr>
+<?php }
+
+get_footer();
+
+?>
